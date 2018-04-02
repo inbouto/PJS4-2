528e747c10642959600371e38990f0defe3e6935
inbouto <guy0001@hotmail.fr>
2018-04-02 02:31:17 +0200

    forgot that one

--- src/donnees/DonneesOracle.java
+++ src/donnees/DonneesOracle.java
@@ -324,4 +324,17 @@ public class DonneesOracle implements IDonnees{
 		
 	}
 	
+	@Override
+	public void setAIName(String cid, String name) {
+		try {
+			Statement st = c.createStatement();
+			st.executeUpdate("update classifier set nomClassifier='" + name + "' where cid='" + cid + "'");
+			
+		} catch (SQLException e) {
+			System.err.println("Erreur requête : setAIName");
+			e.printStackTrace();
+		} 
+		
+	}
+	
 }
